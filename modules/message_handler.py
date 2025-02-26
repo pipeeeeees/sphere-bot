acb1f1a9e7c4bb59e10de2e686a72e64ab1e5838
David Pipes <pipeeeeees@gmail.com>
2025-02-26 00:11:52 -0500

    added commands

--- modules/message_handler.py
+++ modules/message_handler.py
@@ -2,8 +2,12 @@ import json
 import io
 import discord
 import logging
+import datetime
+
+import pollen
 
 logger = logging.getLogger(__name__)
+start_time = datetime.datetime.now()
 
 SCHEDULE_FILE = "config/schedule.json"
 
@@ -17,7 +21,9 @@ def read_json(path):
         return {"reminders": []}
 
 async def handle_message(bot, message, log_channel_id):
-    """Handles messages for the bot, including the $schedule command."""
+    """Handles messages for the bot"""
+
+    # if $schedule is sent in the bot-testing channel, send the schedule file
     if message.content.strip() == "$schedule" and int(message.channel.id) == int(log_channel_id):
         schedule_data = read_json(SCHEDULE_FILE)
         schedule_text = json.dumps(schedule_data, indent=4)
@@ -25,3 +31,24 @@ async def handle_message(bot, message, log_channel_id):
         file = discord.File(io.BytesIO(schedule_text.encode()), filename="schedule.json")
         await message.channel.send("📂 **Schedule file:**", file=file)
         logger.info("✅ Sent schedule file.")
+
+    
+    # if $uptime is sent, send the uptime
+    if message.content.strip() == "$uptime":
+        current_time = datetime.datetime.now()
+        uptime = current_time - start_time
+        await message.channel.send(f"⏳ **Uptime:** {uptime}")
+        logger.info("✅ Sent uptime.")
+
+    # if $pollen is sent, send the pollen count
+    if message.content.strip() == "$pollen":
+        pollen_count = pollen.get_atl_pollen_count()
+        if type(pollen_count) == int:
+            await message.channel.send(f"🌼 **Pollen count:** {pollen_count}")
+            logger.info("✅ Sent pollen count.")
+        elif pollen_count == None:
+            await message.channel.send("❌ **Pollen count not reported.**")
+            logger.info("❌ Pollen count not reported.")
+        elif pollen_count == 'HTML Failure':
+            await message.channel.send("❌ **HTML Parsing Error.**")
+            logger.info("❌ HTML Parsing Error.")
---
+++ modules/pollen.py
@@ -0,0 +1,31 @@
+import webscraper as ws
+
+def get_atl_pollen_count():
+    mylist = ws.chunk_parser(ws.scrape('https://www.atlantaallergy.com/pollen_counts'),
+                                       'class="pollen-num"').split(' ')
+    if len(mylist) > 0:
+        for i in mylist:
+            try:
+                j = int(i)
+                return j
+            except:
+                continue
+    if mylist == ['']:
+        print(mylist)
+        return None
+    else:
+        return 'HTML Failure'
+
+def result_handler():
+    result = get_atl_pollen_count()
+    if type(result) == int:
+        return f"The pollen count in Atlanta for the day is {result}"
+    elif result == None:
+        return "The pollen count in Atlanta has not been reported yet. Please try again later.\n\nNote: Atlanta's pollen count is not reported on the weekends (outside of pollen season).\nhttps://www.atlantaallergy.com/pollen_counts"
+    elif result == 'HTML Failure':
+        return "HTML Parsing Error"
+    else:
+        return "something broke lol"
+    
+if __name__ == '__main__':
+    print(result_handler())
