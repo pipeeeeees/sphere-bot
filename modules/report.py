d14d90edd09339846ef6b5545aed7fe68fe65530
pipeeeeees <pipeeeeees@gmail.com>
2025-03-03 23:29:29 -0500

    Update report.py

--- modules/report.py
+++ modules/report.py
@@ -18,9 +18,9 @@ def get_morning_report():
 
     pollen_count = pollen.get_atl_pollen_count()
     if type(pollen_count) == int:
-        morning_report_str += f"\n  🌼 [Pollen count](https://www.atlantaallergy.com/pollen_counts): **{pollen_count}**"
-    #elif pollen_count == None:
-    #    morning_report_str += "\n  🌼 Pollen count is yet reported"
+        morning_report_str += f"\n  [🌼](https://www.atlantaallergy.com/pollen_counts) Pollen count: **{pollen_count}**"
+    elif pollen_count == None:
+        morning_report_str += "\n  [🌼 Pollen count](https://www.atlantaallergy.com/pollen_counts) is not yet reported"
         
 
     sunset = weather.get_sun_time(0, "sunset")
@@ -33,7 +33,7 @@ def get_morning_report():
     # get current weather
     current_weather = weather.get_current_weather()
     if current_weather != None:
-        morning_report_str += "\n### Here are the [Current Weather Conditions](https://api.weather.gov/stations/KATL/observations/latest):"
+        morning_report_str += "\n### Here are the Current Weather Conditions:"
         morning_report_str += f"\n  🌡️ Temperature: **{current_weather['temperature']}°F**"
         morning_report_str += f"\n  💧 Humidity: **{current_weather['humidity']}%**"
         morning_report_str += f"\n  💨 Wind speed: **{current_weather['wind_speed']} mph**"
