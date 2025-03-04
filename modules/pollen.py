0aeac8013794a8eb827c3f155d24b552b94e00e6
pipeeeeees <pipeeeeees@gmail.com>
2025-03-03 22:23:04 -0500

    Update pollen.py

--- modules/pollen.py
+++ modules/pollen.py
@@ -7,6 +7,33 @@ import asyncio
 DATA_DIR = "modules/pollen_data"
 DATA_FILE = os.path.join(DATA_DIR, "pollen_counts.csv")
 
+def get_atl_pollen_count():
+    mylist = ws.chunk_parser(ws.scrape('https://www.atlantaallergy.com/pollen_counts'),
+                                       'class="pollen-num"').split(' ')
+    if len(mylist) > 0:
+        for i in mylist:
+            try:
+                j = int(i)
+                return j
+            except:
+                continue
+    if mylist == ['']:
+        print(mylist)
+        return None
+    else:
+        return 'HTML Failure'
+
+def result_handler():
+    result = get_atl_pollen_count()
+    if type(result) == int:
+        return f"The pollen count in Atlanta for the day is {result}"
+    elif result == None:
+        return "The pollen count in Atlanta has not been reported yet. Please try again later.\n\nNote: Atlanta's pollen count is not reported on the weekends (outside of pollen season).\nhttps://www.atlantaallergy.com/pollen_counts"
+    elif result == 'HTML Failure':
+        return "HTML Parsing Error"
+    else:
+        return "something broke lol"
+
 def ensure_data_dir():
     os.makedirs(DATA_DIR, exist_ok=True)
     if not os.path.exists(DATA_FILE):
