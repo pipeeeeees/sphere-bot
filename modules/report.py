9acdbe60b24b7114d00f617da7312460a45613d7
pipeeeeees <pipeeeeees@gmail.com>
2025-03-10 23:17:15 -0400

    Update report.py

--- modules/report.py
+++ modules/report.py
@@ -20,17 +20,7 @@ def get_morning_report():
         morning_report_str += f"\n  🌼 Pollen count: **{pollen_count}**"
     elif pollen_count == None:
         morning_report_str += "\n  🌼 Pollen count is not yet reported"
-        
-
-    sunset = weather.get_sun_time(0, "sunset")
-    sunset_string = f"\n  🌅 Sunset is at **{sunset}**"
-    sunrise = weather.get_sun_time(1, "sunrise")
-    sunrise_string = f"\n  🌅 Sunrise tomorrow is at **{sunrise}**"
-    morning_report_str = morning_report_str + sunset_string
-    morning_report_str = morning_report_str + sunrise_string
-
     
-
     # get current weather
     current_weather = weather.get_current_weather()
     if current_weather != None:
@@ -39,6 +29,13 @@ def get_morning_report():
     #    morning_report_str += f"\n  💧 Humidity: **{current_weather['humidity']}%**"
     #    morning_report_str += f"\n  💨 Wind speed: **{current_weather['wind_speed']} mph**"
         morning_report_str += f"\n  🌦️ Conditions: **{current_weather['conditions']}**"
+
+    sunset = weather.get_sun_time(0, "sunset")
+    sunset_string = f"\n  🌅 Sunset is at **{sunset}**"
+    sunrise = weather.get_sun_time(1, "sunrise")
+    sunrise_string = f"\n  🌅 Sunrise tomorrow is at **{sunrise}**"
+    morning_report_str = morning_report_str + sunset_string
+    morning_report_str = morning_report_str + sunrise_string
 
     ascii_plot = weather.get_hourly_temperatures_ascii_plot()
     if ascii_plot:
